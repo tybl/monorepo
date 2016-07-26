03f363c5d8241ca7d5032d9189d15aba4beeff6c
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-07-26 07:12:05 -0400

    Initial makefile and source code

---
+++ Makefile
@@ -0,0 +1,46 @@
+CC := clang
+TARGET := goon
+OBJECTS = $(SOURCES:src/%.cpp=$(BLDDIR)/%.o)
+DEPS = $(OBJECTS:.o=.d)
+INCLUDES := -I src
+SOURCES := src/main.cpp
+
+ifndef CONFIG
+   CONFIG=Debug
+endif
+
+ifeq ($(CONFIG), Debug)
+   BLDDIR := build/debug
+   CFLAGS := -g3 -O0 -fsanitize=address
+   LFLAGS := -g3 -O0 -fsanitize=address
+endif
+
+ifeq ($(CONFIG), Valgrind)
+   BLDDIR := build/valgrind
+   CFLAGS := -g3 -O0
+   LFLAGS := -g3 -O0
+endif
+
+ifeq ($(CONFIG), Release)
+   BLDDIR := build/release
+   CFLAGS := -g0 -O3
+   LFLAGS := -g0 -O3
+endif
+
+CFLAGS += -c -std=c++14 -stdlib=libc++ -Weverything -Wno-c++98-compat -pedantic -MP -MMD
+LFLAGS += -lc++ -lc++abi -lSDL2
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(LFLAGS) -o $@ $(OBJECTS)
+
+$(BLDDIR):
+	mkdir -p $(dir $(OBJECTS))
+
+$(BLDDIR)/%.o: src/%.cpp | $(BLDDIR)
+	$(CC) $(CFLAGS) $(INCLUDES) -o $@ $<
+
+.PHONY: clean
+clean:
+	$(RM) -r build $(TARGET)
+
+-include $(DEPS)
---
+++ src/main.cpp
@@ -0,0 +1,215 @@
+//Using SDL, SDL_image, standard IO, and strings
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <string>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//The window we'll be rendering to
+static SDL_Window* gWindow = NULL;
+
+//The window renderer
+static SDL_Renderer* gRenderer = NULL;
+
+int main(int argc, char* argv[]) {
+   static_cast<void>(argc);
+   static_cast<void>(argv);
+
+   //Initialize SDL
+   if (0 > SDL_Init(SDL_INIT_VIDEO)) {
+      fprintf(stderr, "SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+      return -1;
+   }
+
+   SDL_EventState(SDL_MOUSEMOTION, SDL_IGNORE);
+   SDL_EventState(SDL_FINGERMOTION, SDL_IGNORE);
+
+   //Set texture filtering to linear
+   if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+      printf( "Warning: Linear texture filtering not enabled!");
+   }
+
+   //Create window
+   gWindow = SDL_CreateWindow("goon", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+   if (gWindow == NULL) {
+      fprintf(stderr, "Window could not be created! SDL Error: %s\n", SDL_GetError());
+      return -1;
+   }
+
+   //Create vsynced renderer for window
+   gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+   if (gRenderer == NULL) {
+      printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+      return -1;
+   }
+   //Initialize renderer color
+   SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
+
+   bool keep_running = true;
+   while (keep_running) {
+      SDL_Event e;
+      while (0 != SDL_PollEvent(&e)) {
+         switch (e.type) {
+            case SDL_QUIT:
+               keep_running = false;
+               break;
+            case SDL_WINDOWEVENT:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_SYSWMEVENT:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_APP_TERMINATING:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_APP_LOWMEMORY:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_APP_WILLENTERBACKGROUND:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_APP_DIDENTERBACKGROUND:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_APP_WILLENTERFOREGROUND:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_APP_DIDENTERFOREGROUND:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_KEYDOWN:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_KEYUP:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_TEXTEDITING:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_TEXTINPUT:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_KEYMAPCHANGED:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_MOUSEBUTTONDOWN:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_MOUSEBUTTONUP:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_MOUSEWHEEL:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_JOYAXISMOTION:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_JOYBALLMOTION:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_JOYHATMOTION:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_JOYBUTTONDOWN:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_JOYBUTTONUP:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_JOYDEVICEADDED:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_JOYDEVICEREMOVED:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_CONTROLLERAXISMOTION:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_CONTROLLERBUTTONDOWN:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_CONTROLLERBUTTONUP:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_CONTROLLERDEVICEADDED:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_CONTROLLERDEVICEREMOVED:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_CONTROLLERDEVICEREMAPPED:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_FINGERDOWN:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_FINGERUP:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_DOLLARGESTURE:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_DOLLARRECORD:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_MULTIGESTURE:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_CLIPBOARDUPDATE:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_DROPFILE:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_AUDIODEVICEADDED:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_AUDIODEVICEREMOVED:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_RENDER_TARGETS_RESET:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_RENDER_DEVICE_RESET:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_USEREVENT:
+               printf("Event %d\n", __LINE__);
+               break;
+            case SDL_LASTEVENT:
+               printf("Event %d\n", __LINE__);
+               break;
+#if 0
+            case SDL_MOUSEMOTION:
+               printf("Mouse motion!\n");
+               break;
+            case SDL_FINGERMOTION:
+               printf("Finger motion\n");
+               break;
+#endif
+            default:
+               printf("Something happened!\n");
+               break;
+         } // switch
+      } // while
+
+      //Clear screen
+      SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
+      SDL_RenderClear(gRenderer);
+
+      //Update screen
+      SDL_RenderPresent(gRenderer);
+   } // while (keep_running)
+
+   //Destroy window
+   SDL_DestroyRenderer(gRenderer);
+   SDL_DestroyWindow(gWindow);
+   gWindow = NULL;
+   gRenderer = NULL;
+
+   //Quit SDL subsystems
+   SDL_Quit();
+   return 0;
+}
