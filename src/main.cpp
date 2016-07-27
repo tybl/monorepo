24f3fe6df41e612431af4a72123d96482d582ee0
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-07-27 18:58:33 -0400

    Added more events

--- src/main.cpp
+++ src/main.cpp
@@ -163,11 +163,47 @@ int main(int argc, char* argv[]) {
                   case SDLK_x: printf("x"); break;
                   case SDLK_y: printf("y"); break;
                   case SDLK_z: printf("z"); break;
+                  case SDLK_BACKQUOTE: printf("`"); break;
                   case SDLK_BACKSLASH: printf("\\"); break;
                   case SDLK_BACKSPACE: printf("<backspace>"); break;
+                  case SDLK_COMMA: printf(","); break;
+                  case SDLK_DELETE: printf("<delete>"); break;
+                  case SDLK_DOWN: printf("<down arrow>"); break;
+                  case SDLK_END: printf("<end>"); break;
+                  case SDLK_EQUALS: printf("="); break;
                   case SDLK_ESCAPE: printf("<escape>"); break;
-                  case SDLK_RETURN: printf("<return>"); break;
-                  case SDLK_SPACE: printf("<space>"); break;
+                  case SDLK_F1: printf("<F1>"); break;
+                  case SDLK_F2: printf("<F2>"); break;
+                  case SDLK_F3: printf("<F3>"); break;
+                  case SDLK_F4: printf("<F4>"); break;
+                  case SDLK_F5: printf("<F5>"); break;
+                  case SDLK_F6: printf("<F6>"); break;
+                  case SDLK_F7: printf("<F7>"); break;
+                  case SDLK_F8: printf("<F8>"); break;
+                  case SDLK_F9: printf("<F9>"); break;
+                  case SDLK_F10: printf("<F10>"); break;
+                  case SDLK_F11: printf("<F11>"); break;
+                  case SDLK_F12: printf("<F12>"); break;
+                  case SDLK_F13: printf("<F13>"); break;
+                  case SDLK_F14: printf("<F14>"); break;
+                  case SDLK_F15: printf("<F15>"); break;
+                  case SDLK_F16: printf("<F16>"); break;
+                  case SDLK_F17: printf("<F17>"); break;
+                  case SDLK_F18: printf("<F18>"); break;
+                  case SDLK_F19: printf("<F19>"); break;
+                  case SDLK_F20: printf("<F20>"); break;
+                  case SDLK_F21: printf("<F21>"); break;
+                  case SDLK_F22: printf("<F22>"); break;
+                  case SDLK_F23: printf("<F23>"); break;
+                  case SDLK_F24: printf("<F24>"); break;
+                  case SDLK_HOME: printf("<home>"); break;
+                  case SDLK_LALT: printf("<l alt>"); break;
+                  case SDLK_LCTRL: printf("<l ctrl>"); break;
+                  case SDLK_LEFT: printf("<left arrow>"); break;
+                  case SDLK_LEFTBRACKET: printf("["); break;
+                  case SDLK_LGUI: printf("<l gui>"); break;
+                  case SDLK_LSHIFT: printf("<l shift>"); break;
+                  case SDLK_MINUS: printf("-"); break;
                   case SDLK_NUMLOCKCLEAR: printf("<numlock>"); break;
                   case SDLK_PAGEDOWN: printf("<page down>"); break;
                   case SDLK_PAGEUP: printf("<page up>"); break;
@@ -175,12 +211,20 @@ int main(int argc, char* argv[]) {
                   case SDLK_PERIOD: printf("."); break;
                   case SDLK_PRINTSCREEN: printf("<print screen>"); break;
                   case SDLK_QUOTE: printf("'"); break;
+                  case SDLK_RALT: printf("<r alt>"); break;
+                  case SDLK_RCTRL: printf("<r ctrl>"); break;
+                  case SDLK_RETURN: printf("<return>"); break;
+                  case SDLK_RETURN2: printf("<return2>"); break;
+                  case SDLK_RGUI: printf("<r gui>"); break;
                   case SDLK_RIGHT: printf("<right arrow>"); break;
                   case SDLK_RIGHTBRACKET: printf("]"); break;
                   case SDLK_RSHIFT: printf("<r shift>"); break;
                   case SDLK_SCROLLLOCK: printf("<scroll lock>"); break;
                   case SDLK_SEMICOLON: printf(";"); break;
                   case SDLK_SLASH: printf("/"); break;
+                  case SDLK_SPACE: printf("<space>"); break;
+                  case SDLK_TAB: printf("<tab>"); break;
+                  case SDLK_UP: printf("<up arrow>"); break;
                   case SDLK_AC_BACK:
                   case SDLK_AC_BOOKMARKS:
                   case SDLK_AC_FORWARD:
@@ -196,6 +240,8 @@ int main(int argc, char* argv[]) {
                   case SDLK_AUDIOPLAY:
                   case SDLK_AUDIOPREV:
                   case SDLK_AUDIOSTOP:
+                  default:
+                     printf("unknown key event");
                      break;
                }
                std::fflush(stdout);
@@ -211,10 +257,10 @@ int main(int argc, char* argv[]) {
                printf("Event %d\n", __LINE__);
                break;
             case SDL_MOUSEBUTTONDOWN:
-               printf("Event %d\n", __LINE__);
+               printf("Mouse button down\n");
                break;
             case SDL_MOUSEBUTTONUP:
-               printf("Event %d\n", __LINE__);
+               printf("Mouse button up\n");
                break;
             case SDL_MOUSEWHEEL:
                printf("Event %d\n", __LINE__);
@@ -259,10 +305,10 @@ int main(int argc, char* argv[]) {
                printf("Event %d\n", __LINE__);
                break;
             case SDL_FINGERDOWN:
-               printf("Event %d\n", __LINE__);
+               printf("Finger down\n");
                break;
             case SDL_FINGERUP:
-               printf("Event %d\n", __LINE__);
+               printf("Finger up\n");
                break;
             case SDL_DOLLARGESTURE:
                printf("Event %d\n", __LINE__);
