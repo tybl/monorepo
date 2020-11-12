aca69dfa9b79d19927d474649314ffa982d1ae4d
Timothy Lyons <t.brandon.lyons@gmail.com>
2020-11-12 21:47:59 +0000

    Start parsing input data

--- app/extract_stats/main.cpp
+++ app/extract_stats/main.cpp
@@ -1,2 +1,21 @@
 // License: The Unlicense (https://unlicense.org)
-auto main() -> int {}
+
+#include "pugixml.hpp"
+
+#include <iostream>
+
+auto main(int argc, char const* argv[]) -> int {
+  if (2 == argc) {
+    pugi::xml_document doc;
+    if (!doc.load_file(argv[1])) return -1;
+    pugi::xml_node activities = doc.child("TrainingCenterDatabase").child("Activities");
+
+    for (pugi::xml_node activity : activities.children("Activity")) {
+      pugi::xml_node lap = activity.child("Lap");
+      pugi::xml_node track = lap.child("Track");
+      for (pugi::xml_node track_point : track.children("Trackpoint")) {
+        std::cout << track_point.child("Time").child_value() << std::endl;
+      }
+    }
+  }
+}
