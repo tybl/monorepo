6662b83baddc2d86a3f304c9e9684069c505b1be
Timothy Lyons <t.brandon.lyons@gmail.com>
2022-03-21 19:33:51 -0400

    Start defining unit tests

--- libs/libnmea/test/nmea/nmea.cpp
+++ libs/libnmea/test/nmea/nmea.cpp
@@ -2,6 +2,31 @@
 
 #include <doctest/doctest.h>
 
-TEST_CASE("Getting started") {
-  CHECK(true);
+#include <memory>
+#include <string_view>
+
+namespace pnt {
+
+struct nmea {
+  static auto parse(std::string_view input) -> std::unique_ptr<nmea>;
+};
+
+auto nmea::parse(std::string_view input) -> std::unique_ptr<nmea> {
+  using namespace std::literals;
+  if (!input.starts_with("$G"sv) || !input.ends_with("\r\n"sv)) {
+    return nullptr;
+  }
+  return std::make_unique<nmea>();
+}
+
+} // namespace pnt
+
+TEST_CASE("Is a nmea sentence") {
+  auto result = pnt::nmea::parse("$G\r\n");
+  CHECK(result);
+}
+
+TEST_CASE("Isn't a nmea sentence") {
+  auto result = pnt::nmea::parse("$G\n\n");
+  CHECK(!result);
 }
