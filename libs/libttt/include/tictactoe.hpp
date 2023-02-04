9f7dfee10051ea826d6ebae679e875cd4f2850d7
Timothy Lyons <t.brandon.lyons@gmail.com>
2023-02-04 14:52:04 +0000

    Remove unshared items from header

--- libs/libttt/include/tictactoe.hpp
+++ libs/libttt/include/tictactoe.hpp
@@ -1,9 +1,6 @@
 #ifndef TYBL_TICTACTOE
 #define TYBL_TICTACTOE
 
-#include <cstdlib>
-
-extern char mat[3][3], p1, p2;
 void init();
 void display();
 auto input(char /*choice*/, int /*x*/, int /*y*/) -> int;
