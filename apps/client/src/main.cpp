4e691b418a127746e5deb393cee11bfb7c0fe0b6
Timothy Lyons <t.brandon.lyons@gmail.com>
2023-02-09 02:26:49 +0000

    Create board and move classes

--- apps/client/src/main.cpp
+++ apps/client/src/main.cpp
@@ -1,4 +1,10 @@
+#include <fmt/core.h>
+
+#include <iostream>
 #include <string>
+#include <vector>
+
+#include <unistd.h>
 
 struct game_connector {
   virtual ~game_connector() = 0;
@@ -8,8 +14,6 @@ struct game_connector {
 
 game_connector::~game_connector() {}
 
-#include <iostream>
-#include <vector>
 
 class test_connector
   : public game_connector
@@ -45,7 +49,6 @@ public:
   }
 };
 
-#include <unistd.h>
 
 class network_connector
   : public game_connector
@@ -71,6 +74,69 @@ public:
 
 #include <memory>
 
+namespace ttt {
+
+  class move {
+    int m_row;
+    int m_col;
+    char m_player;
+
+  public:
+    move(int row, int col, char player)
+      : m_row(row)
+      , m_col(col)
+      , m_player(player)
+    {
+      if (0 > m_row || m_row > 2) {
+        throw "something";
+      }
+      if (0 > m_col || m_col > 2) {
+        throw "something";
+      }
+      if (m_player != 'X' && m_player != 'O') {
+        throw "something";
+      }
+    }
+
+    auto row() const -> int { return m_row; }
+
+    auto col() const -> int { return m_col; }
+
+    auto player() const -> int { return m_player; }
+
+  };
+
+  class board {
+    char m_board[3][3] = {0};
+
+  public:
+    void display() const {
+      fmt::print("{} | {} | {}\n", m_board[0][0], m_board[0][1], m_board[0][2]);
+      fmt::print("{} | {} | {}\n", m_board[1][0], m_board[1][1], m_board[1][2]);
+      fmt::print("{} | {} | {}\n", m_board[2][0], m_board[2][1], m_board[2][2]);
+    }
+
+    void apply(move m) {
+      m_board[m.row()][m.col()] = m.player();
+    }
+
+    auto has_winner() const -> bool {
+      return (m_board[0][0] == m_board[0][1] && m_board[0][1] == m_board[0][2])
+          || (m_board[1][0] == m_board[1][1] && m_board[1][1] == m_board[1][2])
+          || (m_board[2][0] == m_board[2][1] && m_board[2][1] == m_board[2][2])
+
+          || (m_board[0][0] == m_board[1][0] && m_board[1][0] == m_board[2][0])
+          || (m_board[0][1] == m_board[1][1] && m_board[1][1] == m_board[2][1])
+          || (m_board[0][2] == m_board[1][2] && m_board[1][2] == m_board[2][2])
+
+          || (m_board[0][0] == m_board[1][1] && m_board[1][1] == m_board[2][2])
+          || (m_board[0][2] == m_board[1][1] && m_board[1][1] == m_board[2][0]);
+    }
+
+  }; // class board
+
+} // namespace ttt
+
 class game {
   game_connector& m_connector;
 
@@ -95,6 +161,10 @@ public:
     //    if (!game.finished())
     //       my_move = make_move(game)
     //       sendto(game.remote_ip, my_move)
+  }
+private:
+  ttt::move make_move() {
+    return ttt::move(0,0,'O');
   }
 };
 
