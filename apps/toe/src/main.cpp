ed04c5f4f54e6780e99bb664662fe87e20968ac5
Timothy Lyons <t.brandon.lyons@gmail.com>
2023-03-06 12:37:48 +0000

    start defining json msgs

--- apps/toe/src/main.cpp
+++ apps/toe/src/main.cpp
@@ -1,5 +1,37 @@
 #include "udp_socket.hpp"
 
+#include <nlohmann/json.hpp>
+
+//  {
+//    "request": "abcdef1234",
+//    "player": "X",
+//    "board": [
+//      [
+//        " ",
+//        " ",
+//        " "
+//      ], [
+//        " ",
+//        "O",
+//        " "
+//      ], [
+//        "X",
+//        " ",
+//        "O"
+//      ]
+//    ]
+//  }
+//  {
+//    "response": "abcdef1234",
+//    "row": 0,
+//    "col": 2,
+//    "locations": [ {
+//        "ip": "192.168.86.68",
+//        "port": 5432
+//      }
+//    ]
+//  }
+
 auto main() -> int {
   // Open socket, listen for game requests
   // switch (request)
