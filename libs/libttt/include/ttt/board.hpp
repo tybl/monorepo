d9af6be87cd4a727ae151df74d72bfb74cca2bcc
Brandon Lyons <t.brandon.lyons@gmail.com>
2023-03-10 16:30:19 -0500

    Don't nest std::array

--- libs/libttt/include/ttt/board.hpp
+++ libs/libttt/include/ttt/board.hpp
@@ -13,7 +13,10 @@
 namespace ttt {
 
 class board {
-  std::array<std::array<player,3>,3> m_board;
+  static constexpr uint16_t NUM_ROWS = 3;
+  static constexpr uint16_t NUM_COLS = 3;
+  static constexpr auto NUM_CELLS = static_cast<size_t>(NUM_ROWS * NUM_COLS);
+  std::array<player, NUM_CELLS> m_board;
 
 public:
   board();
@@ -32,6 +35,10 @@ public:
 
   void decode(ttt::player p_player, uint16_t p_value);
 
+private:
+  static auto position_to_index(move p_move) -> size_t {
+    return p_move.row() * 3 + p_move.col();
+  }
 }; // class board
 
 } // namespace ttt
--- libs/libttt/src/board.cpp
+++ libs/libttt/src/board.cpp
@@ -21,50 +21,50 @@ ttt::board::board()
 
 void ttt::board::display() const {
   fmt::print(" {:1} | {:1} | {:1}\n",
-             static_cast<char>(m_board[0][0]),
-             static_cast<char>(m_board[0][1]),
-             static_cast<char>(m_board[0][2]));
+             static_cast<char>(m_board[0]),
+             static_cast<char>(m_board[1]),
+             static_cast<char>(m_board[2]));
   fmt::print("---|---|---\n");
   fmt::print(" {:1} | {:1} | {:1}\n",
-             static_cast<char>(m_board[1][0]),
-             static_cast<char>(m_board[1][1]),
-             static_cast<char>(m_board[1][2]));
+             static_cast<char>(m_board[3]),
+             static_cast<char>(m_board[4]),
+             static_cast<char>(m_board[5]));
   fmt::print("---|---|---\n");
   fmt::print(" {:1} | {:1} | {:1}\n",
-             static_cast<char>(m_board[2][0]),
-             static_cast<char>(m_board[2][1]),
-             static_cast<char>(m_board[2][2]));
+             static_cast<char>(m_board[6]),
+             static_cast<char>(m_board[7]),
+             static_cast<char>(m_board[8]));
 }
 
-void ttt::board::apply(move p_move) { m_board.at(p_move.row()).at(p_move.col()) = p_move.player_id(); }
+void ttt::board::apply(move p_move) { m_board.at(position_to_index(p_move)) = p_move.player_id(); }
 
 auto ttt::board::has_winner() const -> bool {
-  return (m_board[0][0] != player::Empty && m_board[0][0] == m_board[0][1] && m_board[0][1] == m_board[0][2]) ||
-         (m_board[1][0] != player::Empty && m_board[1][0] == m_board[1][1] && m_board[1][1] == m_board[1][2]) ||
-         (m_board[2][0] != player::Empty && m_board[2][0] == m_board[2][1] && m_board[2][1] == m_board[2][2]) ||
-         (m_board[0][0] != player::Empty && m_board[0][0] == m_board[1][0] && m_board[1][0] == m_board[2][0]) ||
-         (m_board[0][1] != player::Empty && m_board[0][1] == m_board[1][1] && m_board[1][1] == m_board[2][1]) ||
-         (m_board[0][2] != player::Empty && m_board[0][2] == m_board[1][2] && m_board[1][2] == m_board[2][2]) ||
-         (m_board[0][0] != player::Empty && m_board[0][0] == m_board[1][1] && m_board[1][1] == m_board[2][2]) ||
-         (m_board[0][2] != player::Empty && m_board[0][2] == m_board[1][1] && m_board[1][1] == m_board[2][0]);
+  return (m_board[0] != player::Empty && m_board[0] == m_board[1] && m_board[1] == m_board[2]) ||
+         (m_board[3] != player::Empty && m_board[3] == m_board[4] && m_board[4] == m_board[5]) ||
+         (m_board[6] != player::Empty && m_board[6] == m_board[7] && m_board[7] == m_board[8]) ||
+         (m_board[0] != player::Empty && m_board[0] == m_board[3] && m_board[3] == m_board[6]) ||
+         (m_board[1] != player::Empty && m_board[1] == m_board[4] && m_board[4] == m_board[7]) ||
+         (m_board[2] != player::Empty && m_board[2] == m_board[5] && m_board[5] == m_board[8]) ||
+         (m_board[0] != player::Empty && m_board[0] == m_board[4] && m_board[4] == m_board[8]) ||
+         (m_board[2] != player::Empty && m_board[2] == m_board[4] && m_board[4] == m_board[6]);
 }
 
 auto ttt::board::is_winner(ttt::player p_player) const -> bool {
-  return (m_board[0][0] == p_player && m_board[0][1] == p_player && m_board[0][2] == p_player) ||
-         (m_board[1][0] == p_player && m_board[1][1] == p_player && m_board[1][2] == p_player) ||
-         (m_board[2][0] == p_player && m_board[2][1] == p_player && m_board[2][2] == p_player) ||
-         (m_board[0][0] == p_player && m_board[1][0] == p_player && m_board[2][0] == p_player) ||
-         (m_board[0][1] == p_player && m_board[1][1] == p_player && m_board[2][1] == p_player) ||
-         (m_board[0][2] == p_player && m_board[1][2] == p_player && m_board[2][2] == p_player) ||
-         (m_board[0][0] == p_player && m_board[1][1] == p_player && m_board[2][2] == p_player) ||
-         (m_board[0][2] == p_player && m_board[1][1] == p_player && m_board[2][0] == p_player);
+  return (m_board[0] == p_player && m_board[1] == p_player && m_board[2] == p_player) ||
+         (m_board[3] == p_player && m_board[4] == p_player && m_board[5] == p_player) ||
+         (m_board[6] == p_player && m_board[7] == p_player && m_board[8] == p_player) ||
+         (m_board[0] == p_player && m_board[3] == p_player && m_board[6] == p_player) ||
+         (m_board[1] == p_player && m_board[4] == p_player && m_board[7] == p_player) ||
+         (m_board[2] == p_player && m_board[5] == p_player && m_board[8] == p_player) ||
+         (m_board[0] == p_player && m_board[4] == p_player && m_board[8] == p_player) ||
+         (m_board[2] == p_player && m_board[4] == p_player && m_board[6] == p_player);
 }
 
 auto ttt::board::encode(ttt::player p_player) const -> uint16_t {
   uint16_t result = 0;
   for (size_t row = 0; row < 3; ++row) {
     for (size_t col = 0; col < 3; ++col) {
-      auto cell = m_board.at(row).at(col);
+      auto cell = m_board.at(row*3+col);
       const uint16_t digit = (cell != player::Empty) ? ((cell == p_player) ? 1 : 2) : 0;
       result = static_cast<uint16_t>(result * 3) + digit;
     }
@@ -78,7 +78,7 @@ void ttt::board::decode(ttt::player p_player, uint16_t p_value) {
     for (size_t col = 2; col < 3; --col) {
       auto cell = p_value % 3;
       p_value /= 3;
-      m_board.at(row).at(col) = (0 != cell) ? ((1 == cell) ? p_player : opponent) : ttt::player::Empty;
+      m_board.at(row*3+col) = (0 != cell) ? ((1 == cell) ? p_player : opponent) : ttt::player::Empty;
     }
   }
 }
