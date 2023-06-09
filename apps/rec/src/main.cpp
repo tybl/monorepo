43088a32fab99e3ef915d01ba62dbe7542d30adc
Brandon Lyons <t.brandon.lyons@gmail.com>
2023-06-09 07:54:59 -0400

    Solve today's spelling bee

---
+++ apps/rec/src/main.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+static constexpr size_t MIN_WORD_LEN = 4;
+static constexpr size_t MAX_LETTER_CNT = 7;
+
+auto is_allowed(std::string p_word) {
+  auto original_length = p_word.size();
+  std::transform(p_word.cbegin(), p_word.cend(), p_word.begin(), [](char p_letter){ return std::toupper(p_letter); });
+  std::sort(p_word.begin(), p_word.end());
+  auto last = std::unique(p_word.begin(), p_word.end());
+  p_word.erase(last, p_word.end());
+  //return (MIN_WORD_LEN <= original_length) && (p_word.size() <= MAX_LETTER_CNT) && std::all_of(p_word.begin(), p_word.end(), [](char p_letter){ return std::isalpha(p_letter); });
+  static std::string target = "AIJMNRU";
+  return p_word == target;
+}
+
+auto main(int argc, char* argv[]) -> int {
+  if (2 > argc) {
+    // TODO(tybl): Print help
+    return -1;
+  }
+  std::ifstream input(argv[1]);
+  while (input.good()) {
+    std::string word;
+    input >> word;
+    if (is_allowed(word)) {
+      std::cout << word << std::endl;
+    }
+  }
+}
+
+// vim: ts=2 sts=2 sw=2 et
