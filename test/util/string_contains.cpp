dfe225288ed23a0f01330d1e3845ea84063c0687
Brandon Lyons <t.brandon.lyons@gmail.com>
2023-07-27 14:23:16 -0400

    #1: Create std::string::contains() replacement

---
+++ test/util/string_contains.cpp
@@ -0,0 +1,13 @@
+#include "util/string_contains.hpp"
+
+#include <doctest/doctest.h>
+
+using namespace std::literals;
+
+TEST_CASE("String contains character") {
+   CHECK(tybl::util::string_contains("apples"s, 'a'));
+}
+
+TEST_CASE("String does not contain character") {
+   CHECK(!tybl::util::string_contains("apples"s, 'b'));
+}
