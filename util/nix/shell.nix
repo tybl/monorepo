10a04a3005a78f91d268f902ced9b6c19609e9ef
Brandon Lyons <t.brandon.lyons@gmail.com>
2023-07-27 12:58:01 -0400

    Remove unused dependencies

--- util/nix/shell.nix
+++ util/nix/shell.nix
@@ -5,15 +5,10 @@ pkgs.mkShell {
     pkgs.cmake
     pkgs.cppcheck
     pkgs.gcc
-    pkgs.git
-    pkgs.graphviz
     pkgs.include-what-you-use
-    pkgs.neovim
   ];
   nativeBuildInputs = [
-    pkgs.boost180.dev
     pkgs.doctest
     pkgs.fmt
-    pkgs.nlohmann_json
   ];
 }
