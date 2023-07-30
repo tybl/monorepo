b7a029eac382e68eb20ef7e773a186c05a9fd421
Brandon Lyons <t.brandon.lyons@gmail.com>
2023-07-30 19:45:10 -0400

    #1: Add projects as submodules

---
+++ utils/nix/shell.nix
@@ -0,0 +1,19 @@
+################################################################################
+# License: The Unlicense (https://unlicense.org)
+# vim:et:ts=2
+################################################################################
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.clang-tools_16
+    pkgs.cmake
+  ];
+  nativeBuildInputs = [
+    pkgs.doctest
+    pkgs.fmt
+    pkgs.howard-hinnant-date
+    pkgs.pugixml
+    pkgs.SDL2
+    pkgs.tomlplusplus
+  ];
+}
