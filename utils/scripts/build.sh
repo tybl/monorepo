30dc64585444be6b7b5a929abf9ec775f1f486c9
Brandon Lyons <t.brandon.lyons@gmail.com>
2023-08-13 18:49:31 -0400

    #11: Move dependencies to util folder

---
+++ utils/scripts/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cmake -S utils/dependencies -B build_deps
+cmake --build build_deps
+cmake -B build_libpnt -DCMAKE_PREFIX_PATH=build_deps
+cmake --build build_libpnt
+ctest --test-dir build_libpnt
