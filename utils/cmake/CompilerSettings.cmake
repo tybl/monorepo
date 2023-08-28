05b00d849ced2d9d077c0d8d81a89dbec3f5e47c
Brandon Lyons <t.brandon.lyons@gmail.com>
2023-08-28 16:46:40 -0400

    Enable deprecated-copy-dtor

--- utils/cmake/CompilerSettings.cmake
+++ utils/cmake/CompilerSettings.cmake
@@ -13,63 +13,63 @@ function(add_warning_if_available compiler_flag)
 endfunction()
 
 set(ALL_COMPILER_FLAGS
-  #address-of-packed-member
+  address-of-packed-member
   all
-  #analyzer-allocation-size
-  #analyzer-deref-before-check
-  #analyzer-exposure-through-unint-copy
-  #analyzer-fd-access-mode-mismatch
-  #analyzer-fd-double-close
-  #analyzer-fd-leak
-  #analyzer-fd-phase-mismatch
-  #analyzer-fd-type-mismatch
-  #analyzer-fd-use-after-close
-  #analyzer-fd-use-without-check
-  #analyzer-imprecise-fp-arithmetic
-  #analyzer-infinite-recursion
-  #analyzer-jump-through-null
-  #analyzer-out-of-bounds
-  #analyzer-putenv-of-auto-var
-  #analyzer-tainted-assertion
-  #analyzer-use-of-uninitialized-value
-  #analyzer-write-to-const
-  #analyzer-write-to-string-literal
+  analyzer-allocation-size
+  analyzer-deref-before-check
+  analyzer-exposure-through-unint-copy
+  analyzer-fd-access-mode-mismatch
+  analyzer-fd-double-close
+  analyzer-fd-leak
+  analyzer-fd-phase-mismatch
+  analyzer-fd-type-mismatch
+  analyzer-fd-use-after-close
+  analyzer-fd-use-without-check
+  analyzer-imprecise-fp-arithmetic
+  analyzer-infinite-recursion
+  analyzer-jump-through-null
+  analyzer-out-of-bounds
+  analyzer-putenv-of-auto-var
+  analyzer-tainted-assertion
+  analyzer-use-of-uninitialized-value
+  analyzer-write-to-const
+  analyzer-write-to-string-literal
   #arith-conversion
   #array-bounds
   #attribute-alias
   #cast-align
   #class-conversion
   #conversion
-  #deprecated-copy
-  #deprecated-copy-dtor
-  #double-promotion
-  #duplicated-branches
-  #duplicated-cond
+  deprecated-copy
+  deprecated-copy-dtor
+  double-promotion
+  duplicated-branches
+  duplicated-cond
   #effc++
   extra
   #format-overflow
   #init-list-lifetime
-  #lifetime
-  #logical-op
-  #maybe-uninitialized
-  #misleading-indentation
-  #mismatched-tags
+  lifetime
+  logical-op
+  maybe-uninitialized
+  misleading-indentation
+  mismatched-tags
   #non-virtual-dtor
   #null-dereference
-  #old-style-cast
+  old-style-cast
   #overloaded-virtual
-  #pedantic
+  pedantic
   #redundant-tags
-  #restrict
-  #return-local-addr
-  #shadow
+  restrict
+  return-local-addr
+  shadow
   #sign-conversion
-  #stringop-overflow
+  stringop-overflow
   #suggest-attribute=const
   #suggest-attribute=pure
-  #uninitialized
-  #unused
-  #useless-cast
+  uninitialized
+  unused
+  useless-cast
 )
 
 check_cxx_compiler_flag(-Weverything HAS_COMPILER_WARNING_everything)
