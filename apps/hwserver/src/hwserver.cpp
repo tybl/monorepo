01086830c2de39846581acf0e62ac83e3db498ee
Brandon Lyons <t.brandon.lyons@gmail.com>
2023-09-04 10:08:30 -0400

    ZMQ Guide: Basics: hwclient

--- apps/hwserver/src/hwserver.cpp
+++ apps/hwserver/src/hwserver.cpp
@@ -21,15 +21,17 @@ int main() {
     zmq::message_t request;
 
     // Wait for next request from client
-    socket.recv(request, zmq::recv_flags::none);
-    std::cout << "Received Hello" << std::endl;
+    auto result = socket.recv(request, zmq::recv_flags::none);
+    if (static_cast<bool>(result)) {
+      std::cout << "Received Hello" << std::endl;
+    }
 
     // Do some 'work'
     sleep(1);
 
     // Send reply back to client
     zmq::message_t reply(5);
-    memcpy(reply.data(), "World", 5);
+    std::memcpy(reply.data(), "World", 5);
     socket.send(reply, zmq::send_flags::none);
   }
   return 0;
